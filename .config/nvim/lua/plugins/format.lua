baa8eb026f12b9ccd481c4520d9b9363cb8fbdee
Balint Orosz <orosz.balint.97@gmail.com>
2023-03-11 14:04:58 +0100

    add format

---
+++ .config/nvim/lua/plugins/format.lua
@@ -0,0 +1,38 @@
+return {
+	{
+		"mhartington/formatter.nvim",
+		config = function()
+			local util = require("formatter.util")
+			require("formatter").setup({
+				-- Enable or disable logging
+				logging = true,
+				-- Set the log level
+				log_level = vim.log.levels.WARN,
+				-- All formatter configurations are opt-in
+				filetype = {
+					lua = {
+						require("formatter.filetypes.lua").stylua,
+					},
+					typescript = {
+						require("formatter.filetypes.typescript").prettier,
+						require("formatter.filetypes.typescript").eslint_d,
+					},
+					typescriptreact = {
+						require("formatter.filetypes.typescript").prettier,
+						require("formatter.filetypes.typescript").eslint_d,
+					},
+					["*"] = {
+						require("formatter.filetypes.any").remove_trailing_whitespace,
+					},
+				},
+			})
+
+			vim.cmd([[
+        augroup FormatAutogroup
+          autocmd!
+          autocmd BufWritePost * FormatWrite
+        augroup END
+      ]])
+		end,
+	},
+}
